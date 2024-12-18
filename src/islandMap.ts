d06e95e0a47c4103c8ee1fe70ad84e83b3256b54
Martijn van Iersel <mvaniersel@gmail.com>
2024-12-18 11:07:41 +0100

    Move re-usable geometry code to utility module

--- src/islandMap.ts
+++ src/islandMap.ts
@@ -1,6 +1,9 @@
-// import the noise functions you need
 import { createNoise2D, NoiseFunction2D } from 'simplex-noise';
 import { assert } from './util/assert';
+import { Vec3 } from './util/geom/vec3';
+import { Point } from './util/geom/point';
+import { clamp } from './util/math';
+import { pointRange } from './util/geom/pointRange';
 
 /*
 
@@ -26,35 +29,12 @@ export type ConfigType = {
 	paletteUrl: string;
 };
 
-type Vec3 = {
-	x: number,
-	y: number,
-	z: number
-};
-
-function dot_product(a: Vec3, b: Vec3) {
-	return a.x * b.x + a.y * b.y + a.z * b.z;
-}
-
-function vec_length(v: Vec3) {
-	return Math.sqrt(v.x * v.x + v.y * v.y + v.z * v.z);
-}
-
-function vec_normalize(v: Vec3) {
-	const len = vec_length(v);
-	return {
-		x: v.x / len,
-		y: v.y / len,
-		z: v.z / len
-	};
-}
-
 export class IslandMap {
 
 	config: ConfigType;
 	noise2D: NoiseFunction2D;
 	palettePromise: Promise<ImageData>;
-	lightSource = vec_normalize({ x: 0.5, y: 0.8, z: -0.3 });
+	lightSource = Vec3.normalize({ x: 0.5, y: 0.8, z: -0.3 });
 
 	constructor(config: ConfigType) {
 		this.config = config;
@@ -96,13 +76,10 @@ export class IslandMap {
 		let h = 0.5 + 0.5 * (val2 + 0.5 * val1 * 0.3 + val0 * 0.2);
 
 		// distance to center
-		// TODO use point class...
-		let dx = ((width / 2) - x);
-		let dy = ((height / 2) - y);
-		let dist = Math.sqrt(dx * dx + dy * dy) / Math.sqrt((width * width / 4) + (height * height / 4));
+		const delta = new Point(width, height).minus({ x, y });
+		let dist = delta.length() / Point.length({x: width / 2, y: height / 2 });
 		
-		// TODO: bound utility function...
-		return Math.max(0, Math.min(1.0, h - dist));
+		return clamp(h - dist, 0.0, 1.0);
 	}
 
 	async generate() {
@@ -115,32 +92,30 @@ export class IslandMap {
 		assert(ctx);
 		const imageData = ctx.createImageData(width, height);
 
-		// TODO: use eachRange
-		for (let y = 0; y < height; ++y) {
-			for (let x = 0; x < width; ++x) {
-				let idx = ((y * width) + x) * 4;
+		for (const { x, y } of pointRange(width, height)) {
 
-				const h = this.getHeight(x, y);
+			let idx = ((y * width) + x) * 4;
 
-				// calculate shadow
-				let light = 0.85;
-				// if (h > 0.148) {
-				if (h > 0) {
-					let dx = h - this.getHeight(x - 1, y);
-					let dy = h - this.getHeight(x, y - 1);
+			const h = this.getHeight(x, y);
 
-					const angle = dot_product(vec_normalize({ x: dx, y:  dy, z: 0.01 }), this.lightSource);
-					light = Math.cos(angle);
-				}
+			// calculate shadow
+			let light = 0.85;
+			// if (h > 0.148) {
+			if (h > 0) {
+				let dx = h - this.getHeight(x - 1, y);
+				let dy = h - this.getHeight(x, y - 1);
 
-				// map value to palette...
-				const pal = Math.floor(h * (palette.width - 1)) * 4;
-
-				imageData.data[idx + 0] = light * palette.data[pal + 0]; // R value
-				imageData.data[idx + 1] = light * palette.data[pal + 1]; // G value
-				imageData.data[idx + 2] = light * palette.data[pal + 2]; // B value
-				imageData.data[idx + 3] = 255; // A value
+				const angle = Vec3.dot_product(Vec3.normalize({ x: dx, y: dy, z: 0.01 }), this.lightSource);
+				light = Math.cos(angle);
 			}
+
+			// map value to palette...
+			const pal = Math.floor(h * (palette.width - 1)) * 4;
+
+			imageData.data[idx + 0] = light * palette.data[pal + 0]; // R value
+			imageData.data[idx + 1] = light * palette.data[pal + 1]; // G value
+			imageData.data[idx + 2] = light * palette.data[pal + 2]; // B value
+			imageData.data[idx + 3] = 255; // A value
 		}
 		return imageData;
 	}
