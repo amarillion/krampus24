ac56a1afe81c58ba84b70204c16f6eba7ab96741
Martijn van Iersel <mvaniersel@gmail.com>
2024-12-20 19:36:00 +0100

    Convert jigsaw cutouts fully to TypeScript, and apply cutouts in phaser

--- src/board/jigsaw-cutout.ts
+++ src/board/jigsaw-cutout.ts
@@ -1,6 +1,36 @@
 import { randomThunk } from './random.js';
 import { always, pipe } from './utils.js';
 
+type MoveType = [ number, number ];
+type LineSegmentType = {
+	command: 'l', args: [ number, number ] 
+}
+type CurveSegmentType = {
+	command: 'c', args: [ number, number, number, number, number, number ] 
+}
+// type PathSegmentType =  LineSegmentType | CurveSegmentType;
+type PathSegmentType = {
+	command: 'c' | 'l',
+	args: number[]
+}
+type PathType = PathSegmentType[];
+type EdgesType = [ PathType, PathType, PathType, PathType ];
+type PieceType = {
+	x: number,
+	y: number,
+	moveArgs: MoveType
+	edges: EdgesType;
+}
+type PiecesType = {
+	piecesX: number,
+	piecesY: number,
+	pieces: PieceType[];
+}
+type JigsawConfig = {
+	piecesX?: number,
+	piecesY?: number,
+	seed?: string
+}
 /* The gold standard horizontal left to right edge used as a basis */
 const STANDARD_EDGE = [
 	curveOf(0.2, 0.1, 0.42, 0.07, 0.42, 0),
@@ -9,9 +39,9 @@ const STANDARD_EDGE = [
 	curveOf(0, 0.075, 0.2, -0.1, 0.43, 0),
 ];
 
-export function JigsawCutout({ piecesX = 5, piecesY = 5, seed = 'apple' } = {}) {
+export function JigsawCutout({ piecesX = 5, piecesY = 5, seed = 'apple' }: JigsawConfig = {}): PiecesType {
 	const random = randomThunk({ seed });
-	const randomly = fn => (value, ...args) => random() < 0.5 ? fn(value, ...args) : value;
+	const randomly = <T>(fn: (...rest: any[]) => T) => (value : T, ...args: any[]) => random() < 0.5 ? fn(value, ...args) : value;
 	const createEdge = pipe(
 		always(STANDARD_EDGE),
 		randomly(rotateCurveStart),
@@ -33,7 +63,7 @@ export function JigsawCutout({ piecesX = 5, piecesY = 5, seed = 'apple' } = {}) 
 	return { piecesX, piecesY, pieces };
 }
 
-function coordinatesOf({ piecesX, piecesY }) {
+function coordinatesOf({ piecesX, piecesY }: { piecesX: number, piecesY: number }) {
 	const pieces = [];
 	for(let y = 0; y < piecesY; y++) {
 		for(let x = 0; x < piecesX; x++) {
@@ -43,27 +73,27 @@ function coordinatesOf({ piecesX, piecesY }) {
 	return pieces;
 }
 
-function createPiece({ createEdge }) {
-	return coordinates => {
+function createPiece({ createEdge }: { createEdge: () => PathType }) {
+	return (coordinates: { x: number, y: number }) : PieceType => {
 		const top = createEdge();
 		const right = createEdge().map(rotateCurve90);
 		const bottom = createEdge().map(rotateCurve90).map(rotateCurve90);
 		const left = createEdge().map(rotateCurve90).map(rotateCurve90).map(rotateCurve90);
-		const edges = [top, right, bottom, left];
+		const edges: EdgesType = [top, right, bottom, left];
 		return { ...coordinates, moveArgs: [0, 0], edges };
 	}
 }
 
-export function rotateCurve90({ args, ...rest }) {
+export function rotateCurve90({ args, ...rest }: PathSegmentType ): PathSegmentType {
 	const [y1, x1, y2, x2, y, x] = args;
 	return { ...rest, args: [-x1, y1, -x2, y2, -x, y] };
 }
 
-function flipCommand({ args, ...rest }) {
+function flipCommand({ args, ...rest }: PathSegmentType): PathSegmentType {
 	return { ...rest, args: args.map(nr => -nr) };
 }
 
-function flipEvenColumnsHorizontal(piece) {
+function flipEvenColumnsHorizontal(piece: PieceType): PieceType {
 	if (piece.x % 2 === 0) { return piece; }
 	const { moveArgs, edges } = piece;
 	const [/* moveX */, moveY] = moveArgs;
@@ -73,7 +103,7 @@ function flipEvenColumnsHorizontal(piece) {
 	return { ...piece, moveArgs: [1, moveY], edges: [topFlipped, right, bottomFlipped, left] };
 }
 
-function flipEvenRowsVertical(piece) {
+function flipEvenRowsVertical(piece: PieceType): PieceType {
 	if (piece.y % 2 === 0) { return piece; }
 	const { moveArgs, edges } = piece;
 	const [moveX] = moveArgs;
@@ -86,19 +116,19 @@ function flipEvenRowsVertical(piece) {
 /**
  * Randomly scatter the three main points of an edge: the dimple center and the point next to it.
  * Assumes 4 curve commands: curve -> half dimple -> half dimple -> curve */
-function moveKeyPoints({ random }) {
+function moveKeyPoints({ random }: { random: () => number }) {
 	return ([
 		{ command: c1, args: [c1X1, c1Y1, c1X2, c1Y2, c1X, c1Y] },
 		{ command: c2, args: [c2X1, c2Y1, c2X2, c2Y2, c2X, c2Y] },
 		{ command: c3, args: [c3X1, c3Y1, c3X2, c3Y2, c3X, c3Y] },
 		{ command: c4, args: [c4X1, c4Y1, c4X2, c4Y2, c4X, c4Y] },
-	]) => {
+	] : PathType) => {
 		const { x: x1, y: y1 } = randomPoint({ random, radius: 0.04 });
 		const { x: x2, y: y2 } = randomPoint({ random, radius: 0.04 });
 		const { x: dx3, y: dy3 } = randomPoint({ random, radius: 0.01 });
 		const x3 = x1 + dx3;
 		const y3 = y1 + dy3;
-		const asPrecise = nr => asPreciseNumber(nr);
+		const asPrecise = (nr: number) => asPreciseNumber(nr);
 		return [
 			{ command: c1, args: [c1X1, c1Y1, c1X2 + x1, c1Y2 + y1, c1X + x1, c1Y + y1].map(asPrecise) },
 			{ command: c2, args: [c2X1, c2Y1, c2X2 + x2, c2Y2 - y1 + y2, c2X - x1 + x2, c2Y - y1 + y2].map(asPrecise) },
@@ -108,7 +138,7 @@ function moveKeyPoints({ random }) {
 	};
 }
 
-function randomPoint({ random, radius = 0.05 }) {
+function randomPoint({ random, radius = 0.05 }: { random: () => number, radius: number }) {
 	const r = radius * Math.sqrt(random());
 	const theta = random() * 2 * Math.PI;
 	const x = asPreciseNumber(r * Math.cos(theta));
@@ -119,7 +149,7 @@ function randomPoint({ random, radius = 0.05 }) {
 /**
  * Flip the curve handle before the dimple between horizontal and vertical orientation
  * Assumes 4 curve commands: curve -> half dimple -> half dimple -> curve */
-export function rotateCurveStart([p1, p2, p3, p4]) {
+export function rotateCurveStart([p1, p2, p3, p4]: [ PathSegmentType, PathSegmentType, PathSegmentType, PathSegmentType ]) {
 	const [rp1, rp2] = rotateCurvePortion({ commands: [p1, p2] });
 	return [rp1, rp2, p3, p4];
 }
@@ -127,7 +157,7 @@ export function rotateCurveStart([p1, p2, p3, p4]) {
 /**
  * Flip the curve handle before the dimple between horizontal and vertical orientation
  * Assumes 4 curve commands: curve -> half dimple -> half dimple -> curve */
-export function rotateCurveEnd([p1, p2, p3, p4]) {
+export function rotateCurveEnd([p1, p2, p3, p4] : [ PathSegmentType, PathSegmentType, PathSegmentType, PathSegmentType ]) {
 	const [rp3, rp4] = rotateCurvePortion({ commands: [p3, p4], isEnd: true });
 	return [p1, p2, rp3, rp4];
 }
@@ -135,7 +165,7 @@ export function rotateCurveEnd([p1, p2, p3, p4]) {
 /**
  * Flip the curve handle before the dimple between horizontal and vertical orientation
  * Assumes 4 curve commands: curve -> half dimple -> half dimple -> curve */
-export function rotateCurvePortion({ commands, isEnd = false }) {
+export function rotateCurvePortion({ commands, isEnd = false }: { commands: [PathSegmentType, PathSegmentType], isEnd?: boolean }) {
 	const [
 		{ command: c1, args: [c1X1, c1Y1, c1X2, c1Y2, c1X, c1Y] },
 		{ command: c2, args: [/**/, c2Y1, c2X2, c2Y2, c2X, c2Y] },
@@ -150,10 +180,10 @@ export function rotateCurvePortion({ commands, isEnd = false }) {
 }
 
 /** Randomize if a dimple sticks or gets cut out */
-function flipDimple(edge, { edgeIsVertical = false } = {}) {
+function flipDimple(edge: PathType, { edgeIsVertical = false } = {}) {
 	const shouldFlip = edgeIsVertical ? isCoordinateX : isCoordinateY;
-	const isBeforeDimple = (commandIndex, argIndex) => commandIndex === 0 && argIndex < 2;
-	const isAfterDimple = (commandIndex, argIndex) => commandIndex === 3 && 1 < argIndex;
+	const isBeforeDimple = (commandIndex: number, argIndex: number) => commandIndex === 0 && argIndex < 2;
+	const isAfterDimple = (commandIndex: number, argIndex: number) => commandIndex === 3 && 1 < argIndex;
 	return edge.map((command, commandIndex) => {
 		const flippedArgs = command.args.map((nr, argIndex) => {
 			const ignore = !shouldFlip(argIndex) ||
@@ -165,30 +195,30 @@ function flipDimple(edge, { edgeIsVertical = false } = {}) {
 	});
 }
 
-function alignEdgesToNeighbor(piece, _, pieces) {
+function alignEdgesToNeighbor(piece: PieceType, _: unknown, pieces: PieceType[]) {
 	const leftNeighbor = pieces.find(({ x, y }) => x === piece.x - 1 && y === piece.y);
 	const topNeighbor = pieces.find(({ x, y }) => x === piece.x && y === piece.y - 1);
 	const pieceAlignedLeft = leftNeighbor ? copyRightEdge({ from: leftNeighbor, to: piece }) : piece;
 	return topNeighbor ? copyBottomEdge({ from: topNeighbor, to: pieceAlignedLeft }) : pieceAlignedLeft;
 }
 
-function copyRightEdge({ from, to }) {
+function copyRightEdge({ from, to }: { from: PieceType, to: PieceType }) {
 	const index = from.x % 2 === 0 ? 1 : 3;
 	return copyEdge({ from, to, index });
 }
 
-function copyBottomEdge({ from, to }) {
+function copyBottomEdge({ from, to }: { from: PieceType, to: PieceType }) {
 	const index = from.y % 2 === 0 ? 2 : 0;
 	return copyEdge({ from, to, index });
 }
 
-function copyEdge({ from: { edges }, to, index }) {
-	const newEdges = [...to.edges];
+function copyEdge({ from: { edges }, to, index }: { from: PieceType, to: PieceType, index: number }): PieceType {
+	const newEdges: EdgesType = [...to.edges];
 	newEdges[index] = edges[index];
 	return { ...to, edges: newEdges };
 }
 
-function cutFirstRowTop(piece) {
+function cutFirstRowTop(piece: PieceType): PieceType {
 	const { x, y, edges } = piece;
 	if (0 < y) { return piece; }
 	const [/**/, ...rest] = edges;
@@ -197,7 +227,7 @@ function cutFirstRowTop(piece) {
 	return { ...piece, edges: [line, ...rest] };
 }
 
-function cutFirstColumnLeft(piece) {
+function cutFirstColumnLeft(piece: PieceType): PieceType {
 	const { x, y, edges } = piece;
 	if (0 < x) { return piece; }
 	const [first, second, third] = edges;
@@ -206,8 +236,8 @@ function cutFirstColumnLeft(piece) {
 	return { ...piece, edges: [first, second, third, line] };
 }
 
-function cutLastColumnRight({ piecesX }) {
-	return piece => {
+function cutLastColumnRight({ piecesX }: { piecesX: number }) {
+	return (piece: PieceType): PieceType => {
 		const { x, y, edges } = piece;
 		if (x < piecesX - 1) { return piece; }
 		const [first, second, third, fourth] = edges;
@@ -221,8 +251,8 @@ function cutLastColumnRight({ piecesX }) {
 	}
 }
 
-function cutLastRowBottom({ piecesY }) {
-	return piece => {
+function cutLastRowBottom({ piecesY }: { piecesY: number }) {
+	return (piece: PieceType): PieceType => {
 		const { x, y, edges } = piece;
 		if (y < piecesY - 1) { return piece; }
 		const [first, second, third, fourth] = edges;
@@ -236,46 +266,46 @@ function cutLastRowBottom({ piecesY }) {
 	}
 }
 
-function scaleToBoard({ piecesX, piecesY }) {
-	return ({ x, y, moveArgs, edges, ...rest }) => {
+function scaleToBoard({ piecesX, piecesY }: { piecesX: number, piecesY: number }) {
+	return ({ x, y, moveArgs, edges, ...rest } : PieceType) : PieceType => {
 		const scaleX = 1 / piecesX;
 		const scaleY = 1 / piecesY;
 
 		const [moveX, moveY] = moveArgs;
-		const moveArgsScaled = [(x + moveX) * scaleX, (y + moveY) * scaleY];
+		const moveArgsScaled: MoveType = [(x + moveX) * scaleX, (y + moveY) * scaleY];
 
-		const scaleArg = (value, index) => index % 2 === 0 ? value * scaleX : value * scaleY;
-		const scaleCommand = ({ args, ...rest }) => ({ ...rest, args: args.map(scaleArg) });
-		const edgesScaled = edges.map(commands => commands.map(scaleCommand))
+		const scaleArg = (value: number, index: number) => index % 2 === 0 ? value * scaleX : value * scaleY;
+		const scaleCommand = ({ args, ...rest }: PathSegmentType): PathSegmentType => ({ ...rest, args: args.map(scaleArg) });
+		const edgesScaled = edges.map(commands => commands.map(scaleCommand)) as EdgesType;
 		return { x, y, ...rest, moveArgs: moveArgsScaled, edges: edgesScaled };
 	}
 }
 
-function isCoordinateX(index) {
+function isCoordinateX(index: number) {
 	return index % 2 === 0;
 }
 
-function isCoordinateY(index) {
+function isCoordinateY(index: number) {
 	return !isCoordinateX(index);
 }
 
-function lineOf(deltaX, deltaY) {
+function lineOf(deltaX: number, deltaY: number): PathSegmentType {
 	return Path({ command: 'l', args: [deltaX, deltaY] });
 }
 
-export function curveOf(x1, y1, x2, y2, toX, toY) {
+export function curveOf(x1: number, y1: number, x2: number, y2: number, toX: number, toY: number) {
 	return Path({ command: 'c', args: [x1, y1, x2, y2, toX, toY] })
 }
 
-function Path({ command, args = [] }) {
+function Path({ command, args = [] }: PathSegmentType) {
 	return { command, args };
 }
 
-export function cutoutIdOf(index) {
+export function cutoutIdOf(index: number) {
 	return `piece-${index + 1}`;
 }
 
-export function toSvg({ pieces }) {
+export function toSvg({ pieces }: PiecesType) {
 	const clipPaths = pieces.map(toClipPath).join('\n');
 	return /* svg */`
 		<svg slot="cutout" width="0" height="0" viewBox="0 0 1 1">
@@ -286,10 +316,10 @@ export function toSvg({ pieces }) {
 	`;
 }
 
-function toClipPath({ x, y, moveArgs, edges }, index) {
+function toClipPath({ x, y, moveArgs, edges }: PieceType, index: number) {
 	const id = cutoutIdOf(index);
-	const toCommand = i => `${i.command} ${i.args.join(' ')}`
-	const toPath = paths => paths.map(toCommand).join(' ');
+	const toCommand = (i: PathSegmentType) => `${i.command} ${i.args.join(' ')}`
+	const toPath = (paths: PathType) => paths.map(toCommand).join(' ');
 	const commands = edges.map(toPath).join(' ');
 	const path = `M ${moveArgs.join(' ')} ${commands} Z`;
 	return /* svg */`
@@ -297,6 +327,6 @@ function toClipPath({ x, y, moveArgs, edges }, index) {
 	`;
 }
 
-function asPreciseNumber(nr, precision = 5) {
+function asPreciseNumber(nr: number, precision = 5) {
 	return Number(nr.toPrecision(precision));
 }
--- src/scenes/Game.ts
+++ src/scenes/Game.ts
@@ -2,6 +2,8 @@ import Phaser from 'phaser';
 import { notNull } from '../util/assert.ts';
 import { IslandMap } from '../islandMap.js';
 import paletteUrl from '../assets/island-palette.png?url';
+import { JigsawCutout } from '../board/jigsaw-cutout.ts';
+import { Point } from '../util/geom/point.ts';
 
 export default class extends Phaser.Scene {
 
@@ -11,32 +13,86 @@ export default class extends Phaser.Scene {
 
 	init() {}
 	preload() {}
-
-	async createImage() {
-		const canvasTexture = notNull(this.textures.createCanvas('island', 800, 800));
-
-		const canvas = canvasTexture.getSourceImage() as HTMLCanvasElement;
-		const context = notNull(canvas.getContext('2d'));
-
+	
+	async createImages() {
 		const islandMap = new IslandMap({ width: 800, height: 800, paletteUrl });
-		const imageData = islandMap.generate();
+		const imageData = await islandMap.generate();
+
+		const clipOutData = JigsawCutout({ piecesX: 8, piecesY: 8, seed: 'cherry' });
+
+		// generate full map.
+		{
+			const canvasTexture = notNull(this.textures.createCanvas('island', 800, 800));
+
+			const canvas = canvasTexture.getSourceImage() as HTMLCanvasElement;
+			const context = notNull(canvas.getContext('2d'));
+
+			context.putImageData(imageData, 0, 0);
+			canvasTexture.refresh(); // only needed in case we're on WebGL...
+		}
+
+		// this.add.image(0, 0, 'island').setOrigin(0).setScale(1.0);
 
 		// Draw image data to the canvas
-		context.putImageData(await imageData, 0, 0);
-		
-		canvasTexture.refresh(); // only needed in case we're on WebGL...
+		const bmp = await createImageBitmap(imageData);
 
-		this.add.image(0, 0, 'island').setOrigin(0).setScale(1.0);
+		for (const piece of clipOutData.pieces) {
+			const canvasTexture = notNull(this.textures.createCanvas(`piece-${piece.x}-${piece.y}`, 800, 800));
+			const canvas = canvasTexture.getSourceImage() as HTMLCanvasElement;
+			const context = notNull(canvas.getContext('2d'));
+			
+			/* set up clip path */
+			
+			context.lineWidth = 2;
+			context.strokeStyle = 'lightgrey';
+
+			context.beginPath();
+			const width = 800;
+			const height = 800;
+			let pos = new Point(piece.moveArgs[0] * width, 
+				piece.moveArgs[1] * height);
+			context.moveTo(pos.x, pos.y);
+			for (const edge of piece.edges) {
+				for (const segment of edge) {
+					if (segment.command === 'l') {
+						pos = pos.plus(new Point(segment.args[0], segment.args[1]).mul(800));
+						context.lineTo(pos.x, pos.y);
+					}
+					else if (segment.command === 'c') {
+						const way1 = pos.plus(new Point(segment.args[0], segment.args[1]).mul(800));
+						const way2 = pos.plus(new Point(segment.args[2], segment.args[3]).mul(800));
+						pos = pos.plus(new Point(segment.args[4], segment.args[5]).mul(800));
+						context.bezierCurveTo(
+							way1.x, way1.y,
+							way2.x, way2.y,
+							pos.x, pos.y)
+					}
+				}
+			}
+			context.stroke();
+			context.clip();
+
+			// use drawImage instead of putImageData, the latter doesn't adhere to clip rect.
+			context.drawImage(bmp, 0, 0);
+			// context.stroke();
+			// context.closePath();
+
+			canvasTexture.refresh(); // only needed in case we're on WebGL...
+			const img = this.add.image(Math.random() * 1200, Math.random() * 800, `piece-${piece.x}-${piece.y}`)
+				.setOrigin(piece.moveArgs[0], piece.moveArgs[1]) // TODO: this sets the anchor to the top-right corner, which is not ideal for rotation...
+				.setScale(1.0);
+			img.setAngle(Math.random() * 360);
+		}
+
 
 		this.add.text(100, 100, 'Phaser 3 - TypeScript - Vite ', {
 			font: '64px Bangers',
 			color: '#7744ff',
 		});
-
 	}
 
 	create() {
 		// async delegate...
-		this.createImage();
+		this.createImages();
 	}
 }
