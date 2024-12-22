b1e870ecf7e13e5c998b2ab1ee591777d3136f48
Martijn van Iersel <mvaniersel@gmail.com>
2024-12-22 09:17:40 +0100

    Put phaser in its own bundle chunk

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,15 @@
-export default {
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			output: {
+				/* Package phaser in a chunk named phaser */
+				manualChunks: {
+					phaser: ['phaser'],
+				},
+			},
+		},
+	},
 	base: "./" // Use relative paths in index.html, makes our app relocatable.
-}
+});
