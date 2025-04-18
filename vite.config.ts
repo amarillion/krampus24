af9c9ab1974c3575bccb3e1513779124a99ff4d0
Martijn van Iersel <mvaniersel@gmail.com>
2025-04-18 09:08:22 +0200

    Add a service worker with vite-plugin-pwa

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+/* eslint-disable camelcase */
+import { defineConfig } from 'vite';
+import { VitePWA } from 'vite-plugin-pwa';
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			output: {
+				/* Package phaser in a chunk named phaser */
+				manualChunks: {
+					phaser: [ 'phaser' ],
+				},
+			},
+		},
+	},
+	base: './', // Use relative paths in index.html, makes our app relocatable.
+	plugins: [
+		VitePWA({
+			registerType: 'autoUpdate',
+			manifest: {
+				name: 'KrampusHack 2024',
+				short_name: 'krampus24',
+				start_url: '/',
+				display: 'standalone',
+				background_color: '#ffffbb',
+				theme_color: '#7744ff',
+				icons: [ {
+					src: '/logo192.png',
+					sizes: '192x192',
+					type: 'image/png',
+				}, {
+					src: '/logo512.png',
+					sizes: '512x512',
+					type: 'image/png',
+				} ],
+			},
+		}),
+	],
+});
